fc5c90357c7b1c2bc16ecacb9188fbacdd016216
Tomer Mark <tomermark1001@gmail.com>
2019-08-02 20:29:39 +0300

    Fixed

--- ConsoleApplication1/ConsoleApplication1/Recursion.cs
+++ ConsoleApplication1/ConsoleApplication1/Recursion.cs
@@ -6,28 +6,8 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    
     class Recursion
     {
-        //טענת כניסה: הפעולה מקבלת מטריצה המייצגת מבוך שלו כניסה אחת ויציאה אחת. מעבר מייצג 1ת אין מעבר מייצג 0
-        // טענת יציאה: הפעולה מדפיסה את המסלול ליציאה מהמבוך
-
-        public static void Maze(int[,] mat)
-    {
-            Maze(mat, 0, 0);
+       
     }
-        //הפעולה מקבלת מטריצה והתחלה
-        private static void Maze(int[,] mat, int i , int j)
-        {
-            if(j!=mat.GetLength(1))//הפעולה תיגמר כאשר הגענו לסוף השורה האחרונה
-            {
-                if(mat[i,j]==1)//במקרה כשזו הדרך
-                {
-                    Console.WriteLine(i+","+j);
-                    Maze(mat, 0, j + 1);
-                }
-                Maze(mat, i + 1, j);//במקרה וזו לא הדרך. אין טעם לבדוק תקינות בשורה, משום שמניחים כי יש דרך כלשהי
-            }
-        }
-}
 }
---
+++ ConsoleApplication1/ConsoleApplication1/RecursionFixed.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class RecursionFixed
+    {
+        public static void Maze(int[,] mat)
+        {
+            for (int i = 0; i < mat.GetLength(0); i++)//הכניסה
+            {
+                if (mat[i, 0] == 1)
+                    Maze(mat, i, 0);
+            }
+        }
+
+        public static void Maze(int[,] mat, int i, int j)
+        {
+            if (j < mat.GetLength(1) && j >= 0 && i < mat.GetLength(0) && i >= 0)// בדיקה שלא חרגתי מהמערך
+            {
+                if (mat[i, j] == 1)
+                {
+                    Console.WriteLine(i + "," + j);
+                    Maze(mat, i + 1, j);
+                    Maze(mat, i - 1, j);
+                    Maze(mat, i, j + 1);
+                    Maze(mat, i, j - 1);
+                }
+            }
+        }
+    }
+}
